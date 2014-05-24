a45ae95ac5bfd3006a52218ddabbfaccc7d39ff3
jeffroche <jeff.roche@gmail.com>
2014-05-24 13:38:56 -0400

    Using dropbox delta

--- dropcms.py
+++ dropcms.py
@@ -1,144 +1,112 @@
-import os
-import dropbox
-import markdown
-import argparse
+from dropbox.client import DropboxClient
+from flask import Flask, abort, request
+from hashlib import sha256
+import hmac
 import json
+from markdown import markdown
+import os
+import redis
+import threading
+
+if os.environ.get('HEROKU'):
+    redis_url = os.environ['REDISTOGO_URL']
+    redis_client = redis.from_url(redis_url)
+else:
+    redis_client = redis.StrictRedis(host='localhost', port=6379)
+
+# App key and secret from the App console (dropbox.com/developers/apps)
+APP_KEY = os.environ['APP_KEY']
+APP_SECRET = os.environ['APP_SECRET']
+
+app = Flask(__name__)
+app.debug = True
+
+# A random secret used by Flask to encrypt session data cookies
+app.secret_key = os.environ['FLASK_SECRET_KEY']
+
+DROPBOX_TOKEN = os.environ['DROPBOX_TOKEN']
+DROPBOX_ROOT = os.environ['DROPBOX_ROOT']
 
 
-class Page(object):
+def init():
+    """Generate the html and upload to S3
+    """
+    client = DropboxClient(DROPBOX_TOKEN)
+    root = client.metadata(DROPBOX_ROOT)
+    for f in root['contents']:
+        # Ignore deleted files, folders, and non-markdown files
+        if f['is_dir'] or not f['path'].endswith('.md'):
+            continue
 
-    def __init__(self, file_path, dropbox):
-        self.path = file_path
-        f = dropbox.get_file(self.path)
-        self.text = f.read()
-
-    def html(self):
-        html = markdown.markdown(self.text)
-        return html
+        # Convert to Markdown and store as <basename>.html
+        html = markdown(client.get_file(f['path']).read())
+        s3_upload(html, f['path'])
 
 
-class DropCMS(object):
-    """The main manager class which holds the handle to Dropbox and
-    Redis and reloads the data if necessary
+def update():
+    """Generate the html and upload to S3 only for the files that have
+    changed
+    """
+    cursor = redis_client.get('cursor')
+    client = DropboxClient(DROPBOX_TOKEN)
+
+    has_more = True
+
+    while has_more:
+        if cursor:
+            result = client.delta(cursor=cursor, path_prefix=DROPBOX_ROOT)
+        else:
+            result = client.delta(path_prefix=DROPBOX_ROOT)
+
+        for path, metadata in result['entries']:
+
+            # Ignore deleted files, folders, and non-markdown files
+            if (metadata is None or
+                    metadata['is_dir'] or
+                    not path.endswith('.md')):
+                continue
+
+            # Convert to Markdown and store as <basename>.html
+            html = markdown(client.get_file(path).read())
+            # client.put_file(path[:-3] + '.html', html, overwrite=True)
+            s3_upload(html, path)
+
+        # Update cursor
+        cursor = result['cursor']
+        redis_client.set('cursor', cursor)
+
+        # Repeat only if there's more to do
+        has_more = result['has_more']
+
+
+def s3_upload(html, path):
+    print "Loaded %s to S3" % path
+
+
+def validate_request():
+    '''Validate that the request is properly signed by Dropbox.
+       (If not, this is a spoofed webhook.)'''
+
+    signature = request.headers.get('X-Dropbox-Signature')
+    return signature == hmac.new(APP_SECRET, request.data, sha256).hexdigest()
+
+
+@app.route('/webhook')
+def webhook():
+    """Receive a list of changed user IDs from Dropbox and process each.
     """
 
-    VALID_EXTENSIONS = ('.md', '.markdown')
+    # Make sure this is a valid request from Dropbox
+    if not validate_request():
+        abort(403)
 
-    def __init__(self, token, root_folder):
-        self.dropbox = dropbox.client.DropboxClient(token)
-        self.root = root_folder
-
-    def structure(self):
-        """Returns a list of folders with their file contents
-        """
-        s = {
-            'index_page': None,
-            'folders': {}
-        }
-        root_metadata = self.dropbox.metadata(self.root)
-        # Check for index file
-        for item in root_metadata['contents']:
-            name, extn = self.parse_name(item['path'])
-            if name == 'index' and not item['is_dir']:
-                s['index_page'] = {'path': item['path']}
-            elif item['is_dir']:
-                folder_meta = self.dropbox.metadata(item['path'])
-                files = self.get_files(folder_meta['contents'])
-                s['folders'][name] = {
-                    'path': item['path'], 'pages': files
-                }
-        return s
-
-    def get_files(self, contents):
-        """Returns a ``Page`` object for each markdown file in the
-        folder
-        """
-        files = {}
-        for item in contents:
-            if not item['is_dir']:
-                file_name, extn = self.parse_name(item['path'])
-                if extn.lower() in self.VALID_EXTENSIONS:
-                    files[file_name] = {'path': item['path']}
-        return files
-
-    def parse_name(self, path):
-        """Returns the name and extension of the file/folder derived from
-        the full path
-        """
-        file_name_with_extension = os.path.basename(path)
-        file_name, extn = os.path.splitext(file_name_with_extension)
-        return file_name, extn
-
-    def get_root_index(self):
-        """Returns the index page, if there is one, otherwise returns a
-        list of the folders
-        """
-        content = self.structure()
-        index = content['index_page']
-        if index:
-            index_page = Page(content['index_page']['path'], self.dropbox)
-        else:
-            index_page = None
-        folder_names = content['folders'].keys()
-        return index_page, folder_names
-
-    def get_folder_index(self, folder_name):
-        """Returns the index file, if it exists, and the list of files
-        in the folder.
-        """
-        index_page = None
-        content = self.structure()
-        if 'index' in content['folders'][folder_name]['pages']:
-            index_page = content['folders'][folder_name]['pages']['index']
-        page_names = content['folders'][folder_name]['pages'].keys()
-        return index_page, page_names
-
-    def get_page(self, folder_name, page_name):
-        """Loads the page from Dropbox if it's changed,
-        otherwise loads it from Redis
-        """
-        content = self.structure()
-        page_data = content['folders'][folder_name]['pages'][page_name]
-        page = Page(page_data['path'], self.dropbox)
-        return page
-
-
-def cli():
-    args = parse_args()
-    settings = load_settings(args.settings)
-    COMMANDS = ['build', 'run', 's3sync']
-    assert args.command in COMMANDS
-    if args.command == 'build':
-        import freeze
-        freeze.build({
-            "DROPBOX_ACCESS_TOKEN": settings["DROPBOX_TOKEN"],
-            "DROPBOX_ROOT_FOLDER": settings["DROPBOX_ROOT"],
-            "FREEZER_DESTINATION": settings["OUTPUT_DIR"]})
-    elif args.command == 'run':
-        from web import create_app
-        app = create_app({
-            "DROPBOX_ACCESS_TOKEN": settings["DROPBOX_TOKEN"],
-            "DROPBOX_ROOT_FOLDER": settings["DROPBOX_ROOT"]})
-        app.debug = True
-        app.run()
-    elif args.command == 's3sync':
-        app.freeze
-
-
-def load_settings(settings_path):
-    f = open(settings_path)
-    raw = json.loads(f.read())
-    settings = {key.upper(): value for (key, value) in raw.iteritems()}
-    return settings
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description="A tool to generate a static site from dropbox")
-    parser.add_argument(dest='command')
-    parser.add_argument('-s', '--settings', dest='settings')
-    return parser.parse_args()
-
+    for uid in json.loads(request.data)['delta']['users']:
+        # We need to respond quickly to the webhook request, so we do the
+        # actual work in a separate thread. For more robustness, it's a
+        # good idea to add the work to a reliable queue and process the queue
+        # in a worker process.
+        threading.Thread(target=update, args=()).start()
+    return ''
 
 if __name__ == '__main__':
-    cli()
+    app.run(debug=True)
