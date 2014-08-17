d0ad744cae136756e5e489e7ed86c6ca0f128a71
jeffroche <jeff.roche@gmail.com>
2014-08-16 22:07:43 -0400

    Adding beginnings of s3 integration

--- dropcms.py
+++ dropcms.py
@@ -1,11 +1,14 @@
+import boto
 from dropbox.client import DropboxClient
 from flask import Flask, abort, request
 from hashlib import sha256
 import hmac
+from jinja2 import Environment, PackageLoader
 import json
 from markdown import markdown
 import os
 import redis
+# import tempfile
 import threading
 
 if os.environ.get('HEROKU'):
@@ -27,6 +30,13 @@ app.secret_key = os.environ['FLASK_SECRET_KEY']
 DROPBOX_TOKEN = os.environ['DROPBOX_TOKEN']
 DROPBOX_ROOT = os.environ['DROPBOX_ROOT']
 
+template_env = Environment(loader=PackageLoader('web', 'templates'))
+
+# S3
+s3 = boto.connect_s3()
+s3_bucket = s3.get_bucket(os.environ['S3_BUCKET'])
+s3_bucket.set_acl('public-read')
+
 
 def init():
     """Generate the html and upload to S3
@@ -43,20 +53,20 @@ def init():
         s3_upload(html, f['path'])
 
 
-def update():
+def update(init=False):
     """Generate the html and upload to S3 only for the files that have
     changed
     """
-    cursor = redis_client.get('cursor')
+    if init:
+        cursor = None
+    else:
+        cursor = redis_client.get('cursor')
     client = DropboxClient(DROPBOX_TOKEN)
 
     has_more = True
 
     while has_more:
-        if cursor:
-            result = client.delta(cursor=cursor, path_prefix=DROPBOX_ROOT)
-        else:
-            result = client.delta(path_prefix=DROPBOX_ROOT)
+        result = client.delta(cursor=cursor, path_prefix=DROPBOX_ROOT)
 
         for path, metadata in result['entries']:
 
@@ -66,10 +76,15 @@ def update():
                     not path.endswith('.md')):
                 continue
 
-            # Convert to Markdown and store as <basename>.html
-            html = markdown(client.get_file(path).read())
-            # client.put_file(path[:-3] + '.html', html, overwrite=True)
-            s3_upload(html, path)
+            # Extract file name from full path
+            filename = parse_name(path)
+
+            # Convert to Markdown
+            html_raw = markdown(client.get_file(path).read())
+            html = add_template(html_raw)
+
+            # Upload the file to S3
+            s3_upload(html, filename)
 
         # Update cursor
         cursor = result['cursor']
@@ -79,8 +94,36 @@ def update():
         has_more = result['has_more']
 
 
-def s3_upload(html, path):
-    print "Loaded %s to S3" % path
+def s3_upload(html, filename):
+    s3_file_path = "%s/index.html" % filename
+    key = s3_bucket.get_key(s3_file_path)
+    if not key:
+        key = boto.s3.key.Key(s3_bucket)
+        key.key = s3_file_path
+
+    # Write HTML
+    # with tempfile.TemporaryFile() as f:
+    #     f.write(html)
+    #     f.seek(0)
+    #     key.set_contents_from_file(f)
+    # key.set_acl('public-read')
+    key.content_type = 'text/html; charset=UTF-8'
+    key.set_contents_from_string(html, policy='public-read')
+    print "Uploaded %s" % filename
+
+
+def add_template(html):
+    template = template_env.get_template('page.html')
+    return template.render(markup=html)
+
+
+def parse_name(path):
+    """Returns the name and extension of the file/folder derived from
+    the full path
+    """
+    file_name_with_extension = os.path.basename(path)
+    file_name, extn = os.path.splitext(file_name_with_extension)
+    return file_name
 
 
 def validate_request():
